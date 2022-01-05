0a2785491eba9138ac3831f2abff8ef915636be3
e-francis <61321877+e-francis@users.noreply.github.com>
2022-01-05 21:05:59 +0100

    user dashboard updates

---
+++ src/containers/dashboard/index.jsx
@@ -0,0 +1,40 @@
+import { ThemeProvider } from "@material-ui/styles";
+import React from "react";
+import ReactDOM from "react-dom";
+import { navbar } from "../../components/userDashboard";
+import { leftbar } from "../../components/userDashboard";
+import { theme } from "../../components/userDashboard";
+import { feed } from "../../components/userDashboard";
+import { Grid, makeStyles } from "@material-ui/core";
+
+
+const useStyles = makeStyles((theme) => ({
+  right: {
+    [theme.breakpoints.down("sm")]: {
+      display: "none",
+    },
+  },
+}));
+
+const App = () => {
+  const classes = useStyles();
+  return (
+    <div>
+      <Navbar />
+      <Grid container>
+        <Grid item sm={2} xs={2}>
+          <Leftbar />
+        </Grid>
+        <Grid item sm={7} xs={10}>
+          <Feed />
+        </Grid>
+        <Grid item sm={3} className={classes.right}>
+          <Rightbar />
+        </Grid>
+      </Grid>
+      <Add />
+    </div>
+  );
+};
+
+export default dashboard;
